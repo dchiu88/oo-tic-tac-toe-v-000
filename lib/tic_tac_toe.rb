bbccc786c524d96f8427a490624247b926ac311e
David Chiu <davidwchiu@gmail.com>
2018-02-26 00:55:46 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,7 +66,7 @@ def turn
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(index)
-    move(board, index, current_player(board))
+    move(index, current_player)
     display_board(board)
   else
     turn(board)
