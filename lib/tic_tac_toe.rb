aab3129c14809fd343337b0d7af46907c7dfc518
David Chiu <davidwchiu@gmail.com>
2018-02-26 00:50:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -81,8 +81,8 @@ def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
-def turn_count(board)
-  board.count{|token| token == "X" || token == "O"}
+def turn_count
+  @board.count{|token| token == "X" || token == "O"}
 end
 
 def move(index, player = "X")
